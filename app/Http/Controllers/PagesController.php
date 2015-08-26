75fa3bc93e4d3508665aa8eceb84624784eb9cd1
Maxim <maximbeiner@gmail.com>
2015-08-26 14:20:50 +0600

    Add to the main page latest articles, add tape latest comments.

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
+use App\Comment;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -11,6 +13,12 @@ class PagesController extends Controller
 {
     public function index()
     {
-        return view('pages.home');
+        $articles = Article::latest('published_at')->published()->get();
+        $comments = Comment::take(10)->get();
+
+        return view('pages.home')->with([
+            'articles' => $articles,
+            'comments' => $comments
+        ]);
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,5 +4,39 @@
 @stop
 
 @section('content')
-    <h1>Home page</h1>
+    <h1>Laravel blog</h1>
+    <div class="row">
+        <div class="col-md-9">
+            <h2>Latest articles</h2>
+            <hr>
+            @if(!$articles->isEmpty())
+
+                @foreach($articles as $article)
+                    <h3>
+                        <a href="{{ action('ArticlesController@show', ['id' => $article->id]) }}">{{ $article->title }}</a>
+                        <small>Published: {{ $article->created_at->format('D M Y') }}</small>
+                    </h3>
+                @endforeach
+
+            @else
+                <h2>No data</h2>
+            @endif
+        </div>
+        <div class="col-md-3">
+            <h2>Latest comments</h2>
+            <hr>
+            @if(!$comments->isEmpty())
+
+                @foreach($comments as $comment)
+                    <div class="latest-comment">
+                        <h4><a href="{{ action('ArticlesController@show', ['id' => $comment->article->id]) . '#comment_' . $comment->id }}">{{ $comment->author }}: <small>{{ $comment->article->title }}</small></a></h4>
+                        <p>{{ $comment->body }}</p>
+                    </div>
+                @endforeach
+
+            @else
+                <h2>No data</h2>
+            @endif
+        </div>
+    </div>
 @stop
